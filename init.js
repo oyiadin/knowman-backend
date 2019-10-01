8c44a5e70e197ffd935e970a20e7b76bc23b9d0d
oyiadin <oyiadin@gmail.com>
2019-10-01 23:23:49 +0800

    feat: provide init.js to initialize our database

---
+++ init.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const config = require('./config.json');
+mongoose.connect(
+  `${config.db}/${config.database}`,
+  { useNewUrlParser: true });
+
+var models = require('./models');
+
+models.Cat.findOne({ url: 'root' }, (err, cat) => {
+  if (err) {
+    console.log(`Error occurred during finding root-category: ${err}`);
+  } else if (cat) {
+    console.log('Root-category already existed');
+  } else {
+    models.Cat.create({
+      title: 'Root',
+      url: 'root',
+      parent: -1
+    });
+    console.log('Root-category created');
+  }
+});
